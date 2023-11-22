a0dbb91ec3c6981fb4b797d9400340588e945c2f
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2023-11-22 14:31:33 +0100

    Mise en place du projet 5 + conversion et compression images en webp

--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -13,48 +13,88 @@ const Project = () => {
   const { id } = useParams();
   const project = Projects.find((project) => project.id === id);
 
-  return (
-    <main className='project'>
-      <div className='project__header'>
-        <a href='/'>
-          <img className='project__header-arrow' src={theme === 'dark' ? DarkArrow : LightArrow} alt='flèche de retour page home' />
-        </a>
-        <h1 className='project__header-title'>{project.title}</h1>
-      </div>
-      <section className='project__section'>
-        <div className='project__section-description'>
-          <h2><u>Le projet :</u></h2>
-          <p>{project.description}</p>
-          <h3><u>La mission :</u></h3>
-          <p>{project.mission}</p>
-          <h3><u>Les compétences acquises :</u></h3>
-          {project.competences.map((competence, index) =>(
-            <li key={index} className='project__section-description-competences'>
-              {competence}
-            </li>
-          ))}
-          < Tag />
-          <h3>Retrouvez le code source et le site dans les liens ci-dessous :</h3>
-          <div className='project__section-description-links'>
-            <ButtonStyle />
-            <a href={project.github} target='blank'>
-              <button className="project__button theme-color">
-                Code GitHub
-              </button>
-            </a>
-            <a href={project.site} target='blank'>
-              <button className="project__button theme-color">
-                Démo du site
-              </button>
-            </a>
+  if (project.site === undefined || null) {
+    return (
+      <main className='project'>
+        <div className='project__header'>
+          <a href='/'>
+            <img className='project__header-arrow' src={theme === 'dark' ? DarkArrow : LightArrow} alt='flèche de retour page home' />
+          </a>
+          <h1 className='project__header-title'>{project.title}</h1>
+        </div>
+        <section className='project__section'>
+          <div className='project__section-description'>
+            <h2><u>Le projet :</u></h2>
+            <p>{project.description}</p>
+            <h3><u>La mission :</u></h3>
+            <p>{project.mission}</p>
+            <h3><u>Les compétences acquises :</u></h3>
+            {project.competences.map((competence, index) =>(
+              <li key={index} className='project__section-description-competences'>
+                {competence}
+              </li>
+            ))}
+            < Tag />
+            <h3>Retrouvez le code source dans le lien ci-dessous :</h3>
+            <div className='project__section-description-links'>
+              <ButtonStyle />
+              <a href={project.github} target='blank'>
+                <button className="project__button theme-color">
+                  Code GitHub
+                </button>
+              </a>
+            </div>
           </div>
+          <div className='project__section-carrousel'>
+            <Carrousel />
+          </div>
+        </section>
+      </main>
+    )
+  } else {
+    return (
+      <main className='project'>
+        <div className='project__header'>
+          <a href='/'>
+            <img className='project__header-arrow' src={theme === 'dark' ? DarkArrow : LightArrow} alt='flèche de retour page home' />
+          </a>
+          <h1 className='project__header-title'>{project.title}</h1>
         </div>
-        <div className='project__section-carrousel'>
-          <Carrousel />
-        </div>
-      </section>
-    </main>
-  )
+        <section className='project__section'>
+          <div className='project__section-description'>
+            <h2><u>Le projet :</u></h2>
+            <p>{project.description}</p>
+            <h3><u>La mission :</u></h3>
+            <p>{project.mission}</p>
+            <h3><u>Les compétences acquises :</u></h3>
+            {project.competences.map((competence, index) =>(
+              <li key={index} className='project__section-description-competences'>
+                {competence}
+              </li>
+            ))}
+            < Tag />
+            <h3>Retrouvez le code source et le site dans les liens ci-dessous :</h3>
+            <div className='project__section-description-links'>
+              <ButtonStyle />
+              <a href={project.github} target='blank'>
+                <button className="project__button theme-color">
+                  Code GitHub
+                </button>
+              </a>
+              <a href={project.site} target='blank'>
+                <button className="project__button theme-color">
+                  Démo du site
+                </button>
+              </a>
+            </div>
+          </div>
+          <div className='project__section-carrousel'>
+            <Carrousel />
+          </div>
+        </section>
+      </main>
+    )
+  }
 }
 
 export default Project
